5874c8a71d37c86fb78aaa6f693796a27c000a7a
diegodiaz <64859991+diodz@users.noreply.github.com>
2020-07-27 16:32:48 -0500

    Finished data_preparation.py code to produce merged dataframe to be used in regression analysis

--- regressions/data_preparation.py
+++ regressions/data_preparation.py
@@ -7,7 +7,32 @@ Created on Mon Jul 27 00:07:42 2020
 import pandas as pd
 
 
+def read_and_process_data():
+    '''
+
+    Returns
+    -------
+    None.
+
+    '''
+    geomet = pd.read_stata("data/IfoGAME_balanced_panel.dta")
+    econ_freedom = read_fraser_institute_data()
+    ef_expanded = add_years(econ_freedom)
+    ef_expanded = interpolate_fraser_data(ef_expanded)
+    merged = geomet.merge(ef_expanded, how='left')
+    merged.to_csv('data/econ_freedom_and_geomet.csv', index=False)
+    return merged
+
+
 def read_fraser_institute_data():
+    '''
+
+    Returns
+    -------
+    econ_freedom : TYPE
+        DESCRIPTION.
+
+    '''
     econ_freedom = pd.read_excel(
         "data/efw-2019-master-index-data-for-researchers.xlsx",
         sheet_name="EFW Panel Data 2019 Report",
@@ -23,16 +48,26 @@ def read_fraser_institute_data():
                                  "Countries": "country"}, inplace=True)
     return econ_freedom
 
-#%%
 
-GeoMet = pd.read_stata("data/IfoGAME_balanced_panel.dta")
-econ_freedom = read_fraser_institute_data()
+def interpolate_fraser_data(df):
+    '''
+    Parameters
+    ----------
+    df : TYPE
+        DESCRIPTION.
 
-df = add_years(econ_freedom)
+    Returns
+    -------
+    None.
 
-merged = GeoMet.merge(econ_freedom, how='left')
+    '''
+    df.sort_values(['iso', 'year'], ascending=[True, True], inplace=True)
+    df = df.reset_index().drop(columns=['index'])
+    df = df.groupby('iso').apply(lambda group: group.interpolate
+                                 (limit_area='inside'))
+    return df
 
-#%%
+
 def add_years(df):
     '''
 
@@ -53,34 +88,31 @@ def add_years(df):
     for row in df_aux.itertuples(index=False):
         dct[row[1]] = row[2]
     for iso in df['iso'].unique():
-        df = df.append({"year": 1979, "iso": iso, "country": dct[iso]},
-                       ignore_index=True)
-        df = df.append({"year": 1978, "iso": iso, "country": dct[iso]},
-                       ignore_index=True)
-        df = df.append({"year": 1977, "iso": iso, "country": dct[iso]},
-                       ignore_index=True)
-        df = df.append({"year": 1976, "iso": iso, "country": dct[iso]},
-                       ignore_index=True)
+        df = add_entries_for_country(df, iso, dct[iso])
     return df
 
-#%%
-def fill_country_names(df):
-    '''
 
+def add_entries_for_country(df, iso_code, country_name):
+    '''
+    Adds years in between.
 
     Parameters
     ----------
     df : TYPE
         DESCRIPTION.
+    iso_code : TYPE
+        DESCRIPTION.
+    country_name : TYPE
+        DESCRIPTION.
 
     Returns
     -------
-    None.
+    df : TYPE
+        DESCRIPTION.
 
     '''
-    dct = {}
-    df_aux = df[df['year' == 1970]]
-    for row in df_aux.itertuples(index=False):
-        dct[row[1]] = row[2]
-    for iso in df['iso'].unique():
-
+    for i in range(1971, 2000):
+        if i % 5 != 0:
+            df = df.append({"year": i, "iso": iso_code, "country": country_name
+                            }, ignore_index=True)
+    return df
--- regressions/fixed_effects_geomet.R
+++ regressions/fixed_effects_geomet.R
@@ -41,7 +41,7 @@ eval(parse(text = getURL(url_robust, ssl.verifypeer = FALSE)),
      envir=.GlobalEnv)
 ########
 
-geomet_ECindex <- read_xlsx("data/geomet_ECindex.xlsx")
+geomet_ECindex <- read.csv("data/econ_freedom_and_geomet.csv")
 geomet_ECindex <- as.data.frame(geomet_ECindex)
 geomet_ECindex$cpi <- as.numeric(geomet_ECindex$cpi)
 geomet_ECindex$lcpi <- log(geomet_ECindex$cpi)
@@ -58,22 +58,22 @@ GeoMet_panel$L.gross <- lag(GeoMet_panel$gross, 1)
 GeoMet_panel$L.fdi <- lag(GeoMet_panel$fdi, 1)
 GeoMet_panel$L.lcpi <- lag(GeoMet_panel$lcpi, 1)
 GeoMet_panel$L.balance <- lag(GeoMet_panel$balance, 1)
-GeoMet_panel$L.ECI_interpol <- lag(GeoMet_panel$ECI_interpol, 1)
+GeoMet_panel$L.EFW <- lag(GeoMet_panel$EFW, 1)
 
 # creating first difference
 GeoMet_panel$D.lcgdp <- diff(GeoMet_panel$lcgdp, 1)
 
 # lagging sub indexes from economic freedom
 GeoMet_panel$L.size_gov <- lag(GeoMet_panel$size_gov, 1)
-GeoMet_panel$L.property_rights <- lag(GeoMet_panel$property_rights, 1)
-GeoMet_panel$L.sound <- lag(GeoMet_panel$sound, 1)
-GeoMet_panel$L.freedom_trade <- lag(GeoMet_panel$freedom_trade, 1)
+GeoMet_panel$L.property_rights <- lag(GeoMet_panel$prop_rights, 1)
+GeoMet_panel$L.sound <- lag(GeoMet_panel$sound_money, 1)
+GeoMet_panel$L.freedom_to_trade <- lag(GeoMet_panel$freedom_to_trade, 1)
 GeoMet_panel$L.regulation <- lag(GeoMet_panel$regulation, 1)
 
-columns <- c('D.lcgdp', 'indexla', 'L.ECI_interpol', 'L.lcgdp', 'L.lpop',
+columns <- c('D.lcgdp', 'indexla', 'L.EFW', 'L.lcgdp', 'L.lpop',
              'L.polity', 'L.open', 'L.interest', 'L.credit', 'L.gross', 'L.fdi',
              'L.lcpi', 'L.balance', 'year', 'L.size_gov', 'L.property_rights',
-             'L.sound', 'L.freedom_trade', 'L.regulation')
+             'L.sound', 'L.freedom_to_trade', 'L.regulation')
 
 GeoMet_panel <- GeoMet_panel[c(columns, 'country')]
 for (col in columns){
@@ -84,7 +84,7 @@ for (col in columns){
 # Fixed effects regression with ECI
 # (Table: 1)
 #----------------------------------------------
-fixed <- plm(D.lcgdp ~ indexla+L.ECI_interpol+L.lcgdp+L.lpop+L.polity+L.open+
+fixed <- plm(D.lcgdp ~ indexla+L.EFW+L.lcgdp+L.lpop+L.polity+L.open+
                L.interest+L.credit+L.gross+L.fdi+L.lcpi+L.balance+factor(year),
                data=GeoMet_panel, index=c("country", "year"), model="within")
 
@@ -96,12 +96,25 @@ print(summary(fixed, robust=TRUE))
 # (Table: 1)
 #----------------------------------------------
 fixed <- plm(D.lcgdp ~ indexla+L.size_gov+L.property_rights+L.sound+
-             L.freedom_trade+L.regulation+L.lcgdp+L.lpop+L.polity+L.open+
+             L.freedom_to_trade+L.regulation+L.lcgdp+L.lpop+L.polity+L.open+
              L.interest+L.credit+L.gross+L.fdi+L.balance+factor(year),
              data=GeoMet_panel, index=c("country", "year"), model="within")
 summary(fixed)
 reg2 <- tidy(fixed)
 print(summary(fixed, robust=TRUE))
+
+#----------------------------------------------
+# Fixed effects regression with ECI subindexes and time trend
+# (Table: Extra)
+#----------------------------------------------
+fixed <- plm(D.lcgdp ~ indexla+year+L.size_gov+L.property_rights+L.sound+
+                 L.freedom_to_trade+L.regulation+L.lcgdp+L.lpop+L.polity+L.open+
+                 L.interest+L.credit+L.gross+L.fdi+L.balance+factor(year),
+             data=GeoMet_panel, index=c("country", "year"), model="within")
+summary(fixed)
+reg2 <- tidy(fixed)
+print(summary(fixed, robust=TRUE))
+
 
 
 
