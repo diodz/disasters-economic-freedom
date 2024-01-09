3c66c0110a15689cb86df5db709f332b2485bbb2
Diego A. Diaz <diegodiaz@uchicago.edu>
2024-01-09 13:33:44 +0000

    Updated flask app on 2024-01-09

--- scripts/data_preparation.py
+++ scripts/data_preparation.py
@@ -322,3 +322,4 @@ def add_entries_for_country(df, iso_code, country_name):
 # Change made on 2024-06-26 18:33:16.738299
 # Change made on 2024-06-26 18:33:18.497766
 # Change made on 2024-06-26 18:33:20.126590
+# Change made on 2024-06-26 18:33:21.810338
